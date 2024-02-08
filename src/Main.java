eb71d854cd7cc3bd1ae952f3950a6de09a902a9e
Rafael-S-Muniz <rsmuniz18@gmail.com>
2024-02-08 18:33:50 -0300

    Criei a plataforma de streaming "RafaFlix" onde já alimentei com um filme e dados básicos iniciais.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o RafaFlix");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("O ano de lançamento do filme é: " + anoDeLancamento);
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 9.2;
+        String sinopse = "Filme de aventura relacionado a aviação";
+        System.out.println(sinopse);
+
+        System.out.println();
+
+    }
+}
