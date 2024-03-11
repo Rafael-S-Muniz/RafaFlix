b09a49b0faddd36e7d9b1e410c2a517bc2ce275b
Rafael-S-Muniz <rsmuniz18@gmail.com>
2024-03-11 16:09:15 -0300

    Update to the Main class, OOP and English language;

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o RafaFlix");
-        System.out.println("Filme: Top Gun: Maverick");
+        System.out.println("Welcome to RafaFlix");
+        Movie movie1 = new Movie();
+        movie1.name = "Top Gun: Maverick";
+        movie1.genre = "Action";
+        movie1.releaseYear = 2022;
+        movie1.averageRate = (7.2 + 6.9 + 9 + 8.2) / 4;
+        movie1.ratingCounts = 4;
 
-        int anoDeLancamento = 2022;
-        System.out.println("O ano de lançamento do filme é: " + anoDeLancamento);
-        boolean incluidoNoPlanoBase = false;
-        double media = (7.2 + 6.9 + 9 + 8.2) / 4;
-        int estrelas = (int) media / 2;
-        String sinopse = "Filme de aventura relacionado a aviação";
+        if (movie1.releaseYear < 2022) {
+            movie1.includedInPlan = true;
+        }
 
-        System.out.println(sinopse);
-        System.out.println("A nota do filme é: " + media);
-        System.out.println("Esse filme é grátis? Resposta: " + incluidoNoPlanoBase);
-        System.out.println("O filme tem " + estrelas + " estrelas");
+
+        System.out.println("Movie: " + movie1.name);
+        System.out.println("The release of this movie was in: " + movie1.releaseYear);
+        System.out.println("Movie rate: " + movie1.averageRate);
+        System.out.println("This film is included at the plan?: " + movie1.includedInPlan);
 
     }
 }
