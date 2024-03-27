24d4eec69486b14a6a10ec961239270649adb1be
Rafael-S-Muniz <rsmuniz18@gmail.com>
2024-03-27 20:02:45 -0300

    Update to the Main class, OOP and English language;

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,16 @@ public class Main {
         movie1.name = "Top Gun: Maverick";
         movie1.genre = "Action";
         movie1.releaseYear = 2022;
-        movie1.averageRate = (7.2 + 6.9 + 9 + 8.2) / 4;
-        movie1.ratingCounts = 4;
+        movie1.ratingCounts = 0;
+        movie1.rate = 0;
+        movie1.avaliation(8);
+        movie1.avaliation(5.7);
+        movie1.avaliation(9.3);
 
-        if (movie1.releaseYear < 2022) {
-            movie1.includedInPlan = true;
+        if (movie1.media() == 0) {
+            System.out.println("There is no avaliations yet.");
         }
-
-
-        System.out.println("Movie: " + movie1.name);
-        System.out.println("The release of this movie was in: " + movie1.releaseYear);
-        System.out.println("Movie rate: " + movie1.averageRate);
-        System.out.println("This film is included at the plan?: " + movie1.includedInPlan);
+        movie1.showMovieDescription();
 
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -3,8 +3,28 @@ public class Movie {
     String genre;
     int releaseYear;
     boolean includedInPlan;
-    double averageRate;
+    double rate;
     double ratingCounts;
     double durationInMinutes;
+
+    void avaliation(double avaliation) {
+        rate += avaliation;
+        ratingCounts++;
+    }
+
+    double media() {
+        if (ratingCounts > 0) {
+            return rate / ratingCounts;
+        } else {
+            return 0;
+        }
+    }
+
+    void showMovieDescription() {
+        System.out.println("The movie title is: " + name);
+        System.out.println("The movie was released at the year of: " + releaseYear);
+        System.out.println("The average rate of the movie is: " + media());
+    }
+
 
 }
